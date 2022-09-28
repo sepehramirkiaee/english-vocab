30a4aa6f17fdab384a144b217bd02581da7e2731
Sepehr Amirkiee <sepehr.amirkiaee@gmail.com>
2022-09-28 19:11:35 +0330

    s

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    allowedHosts: ['englishvocab.com']
+    allowedHosts: ['myvocab.ir']
   },
   pwa: {
     name: 'Vocab',
