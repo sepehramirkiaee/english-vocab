b493193a4c41c83fe3258e9a3ab7117e9875a92a
Sepehr Amirkiee <sepehr.amirkiaee@gmail.com>
2022-09-29 22:39:00 +0330

    s

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import store from './store'
 import './assets/tailwind.css'
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = 'http://myvocab.ir:8088';
+axios.defaults.baseURL = process.env.BASE_URL;
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers['Access-Control-Allow-Origin'] = '*';
 
